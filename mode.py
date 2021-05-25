fee0066b5ff279f1ae4a5a430f25f073e5c7ff3e
Hritik <65850052+hritikaswani@users.noreply.github.com>
2021-05-25 17:32:52 +0530

    Update mode.py

--- mode.py
+++ mode.py
@@ -8,21 +8,38 @@ with open('Graph.csv', newline='') as f:
 file_data.pop(0)
 new_data=[]
 for i in range(len(file_data)):
-    n_num=file_data[i][1]
+    n_num=file_data[i][2]
     new_data.append(float(n_num))
 
 data=Counter(new_data)
 mode_data_for_range={
-                        '50-60':0,'60-70':0,'70-80':0
+                        '75-85':0,'85-95':0,'95-105':0,
+                        '105-115':0,'115-125':0,'125-135':0,
+                        '135-145':0,'145-155':0,'155-165':0,
+                        '165-175':0
                     }
 
-for height,occurance in data.items():
-    if 50<float(height)<60:
-        mode_data_for_range['50-60']+=occurance
-    elif 60<float(height)<70:
-        mode_data_for_range['60-70']+=occurance
-    elif 70<float(height)<80:
-        mode_data_for_range['70-80']+=occurance
+for weight,occurance in data.items():
+    if 75<weight<85:
+        mode_data_for_range['75-85']+=occurance
+    elif 85<weight<95:
+        mode_data_for_range['85-95']+=occurance
+    elif 95<weight<105:
+        mode_data_for_range['95-105']+=occurance
+    elif 105<weight<115:
+        mode_data_for_range['105-115']+=occurance
+    elif 115<weight<125:
+        mode_data_for_range['115-125']+=occurance
+    elif 125<weight<135:
+        mode_data_for_range['125-135']+=occurance
+    elif 135<weight<145:
+        mode_data_for_range['135-145']+=occurance
+    elif 145<weight<155:
+        mode_data_for_range['145-155']+=occurance
+    elif 155<weight<165:
+        mode_data_for_range['155-165']+=occurance
+    elif 165<weight<175:
+        mode_data_for_range['165-175']+=occurance
 
 mode_range , mode_occurance=0,0
 
